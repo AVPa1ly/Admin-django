299a437d154cdbd47ff9b465b9b06a4219fe444f
AVPa1ly <andrewvorobiov@gmail.com>
2022-12-21 15:43:56 +0300

    Add homepage redirect

--- flexair_admin_django/urls.py
+++ flexair_admin_django/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import reverse_lazy
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     path('admin/', admin.site.urls),
 ]
